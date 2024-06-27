97178b9340b47c5e98065c492cf040cbef3651d9
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-27 18:24:20 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_MVC_Core_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
