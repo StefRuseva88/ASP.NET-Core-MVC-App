0b5e3f1efb84aa23a0f30404f4a58726c12e17bc
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-28 20:34:12 +0300

    Add files via upload

---
+++ ViewModels/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_MVC_Core_WebApp.ViewModels.Product
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
