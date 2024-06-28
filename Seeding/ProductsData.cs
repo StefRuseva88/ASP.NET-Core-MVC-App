614c2994da68da63ff8ea49f8c89f825f21bdcb5
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-28 20:32:41 +0300

    Add files via upload

---
+++ Seeding/ProductsData.cs
@@ -0,0 +1,30 @@
+﻿namespace ASP.NET_MVC_Core_WebApp.Seeding;
+using ViewModels.Product;
+
+public static class ProductsData
+{
+    public static IEnumerable<ProductViewModel> Products = new List<ProductViewModel>()
+    {
+        new ProductViewModel()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Cheese",
+            Price = 7.00
+        },
+
+        new ProductViewModel()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Ham",
+            Price = 5.50
+        },
+
+        new ProductViewModel()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Bread",
+            Price = 1.50
+        }
+        
+    };
+}
