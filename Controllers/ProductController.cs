74e468cd6da70413a618e943b374b1bb600bbfe8
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-28 20:31:05 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using static ASP.NET_MVC_Core_WebApp.Seeding.ProductsData;
+
+namespace ASP.NET_MVC_Core_WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult All()
+        {
+            return View(Products);
+        }
+    }
+}
