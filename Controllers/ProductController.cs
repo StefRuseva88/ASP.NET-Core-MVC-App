c336a0a3b49101228c36c551733d7af5065a3925
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-06-29 15:28:07 +0300

    Update ProductController.cs

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using ASP.NET_MVC_Core_WebApp.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 using static ASP.NET_MVC_Core_WebApp.Seeding.ProductsData;
 
@@ -9,6 +11,22 @@ namespace ASP.NET_MVC_Core_WebApp.Controllers
         public IActionResult All()
         {
             return View(Products);
+        }
+
+        public IActionResult ById(string id)
+        {
+            ProductViewModel? product = Products.FirstOrDefault(p => p.Id.ToString().Equals(id));
+            if(product == null)
+            {
+                return this.RedirectToAction("All");
+            }
+           return this.View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            string jsonText = JsonConvert.SerializeObject(Products, Formatting.Indented);
+            return Json(jsonText);
         }
     }
 }
